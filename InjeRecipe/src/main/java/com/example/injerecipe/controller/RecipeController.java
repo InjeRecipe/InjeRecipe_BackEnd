388be895b224cd7bec90ceca423e4d84220b71ff
Dongmin <ehdals45454@naver.com>
2024-02-14 11:34:53 +0900

    fix: controller, service 분리

--- InjeRecipe/src/main/java/com/example/injerecipe/controller/RecipeController.java
+++ InjeRecipe/src/main/java/com/example/injerecipe/controller/RecipeController.java
@@ -1,5 +1,6 @@
 package com.example.injerecipe.controller;
 
+import com.example.injerecipe.service.RecipeService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,147 +23,33 @@ import java.net.URL;
 @RequiredArgsConstructor
 public class RecipeController {
 
-    private String baseUrl = "http://211.237.50.150:7080/openapi";
-
-    private String s = "나물비빔밥";
-    @Value("${openApi.serviceKey}")
-    private String serviceKey;
-
-    @Value("${openApi.dataType}")
-    private String dataType;
-
-    @Value("${openApi.apiUrl}")
-    private String url;
+    private final RecipeService recipeService;
 
     @Operation(summary = "재료정보")
     @GetMapping("/ingredients")
     public ResponseEntity<String> getIngredients(@RequestParam int start, @RequestParam int end){
-        HttpURLConnection urlConnection = null;
-        InputStream stream = null;
-        String result = null;
 
-        String urlStr = baseUrl +
-                "/" + serviceKey +
-                "/" + dataType +
-                "/" + url +
-                "/" + start +
-                "/" + end;
 
-        try {
-            URL url = new URL(urlStr);
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-            stream = getNetworkConnection(urlConnection);
-            result = readStreamToString(stream);
-
-            if (stream != null) stream.close();
-        } catch(IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }
-
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return new ResponseEntity<>(recipeService.getIngredients(start, end), HttpStatus.OK);
     }
 
     @Operation(summary = "과정정보")
     @GetMapping("/steps")
     public ResponseEntity<String> getSteps(@RequestParam int start, @RequestParam int end){
-        HttpURLConnection urlConnection = null;
-        InputStream stream = null;
-        String result = null;
 
-        String urlStr = baseUrl +
-                "/" + serviceKey +
-                "/" + dataType +
-                "/" + url +
-                "/" + start +
-                "/" + end;
 
-        try {
-            URL url = new URL(urlStr);
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-            stream = getNetworkConnection(urlConnection);
-            result = readStreamToString(stream);
-
-            if (stream != null) stream.close();
-        } catch(IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }
-
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return new ResponseEntity<>(recipeService.getSteps(start, end), HttpStatus.OK);
     }
 
     @Operation(summary = "기본정보")
     @GetMapping("/recipes")
     public ResponseEntity<String> getRecipes(@RequestParam int start, @RequestParam int end){
-        HttpURLConnection urlConnection = null;
-        InputStream stream = null;
-        String result = null;
 
-        String urlStr = baseUrl +
-                "/" + serviceKey +
-                "/" + dataType +
-                "/" + url +
-                "/" + start +
-                "/" + end;
-
-        try {
-            URL url = new URL(urlStr);
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-            stream = getNetworkConnection(urlConnection);
-            result = readStreamToString(stream);
-
-            if (stream != null) stream.close();
-        } catch(IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }
-
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return new ResponseEntity<>(recipeService.getRecipes(start, end), HttpStatus.OK);
     }
 
 
-    /* URLConnection 을 전달받아 연결정보 설정 후 연결, 연결 후 수신한 InputStream 반환 */
-    private InputStream getNetworkConnection(HttpURLConnection urlConnection) throws IOException {
-        urlConnection.setConnectTimeout(3000);
-        urlConnection.setReadTimeout(3000);
-        urlConnection.setRequestMethod("GET");
-        urlConnection.setDoInput(true);
 
-        if(urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-            throw new IOException("HTTP error code : " + urlConnection.getResponseCode());
-        }
-
-        return urlConnection.getInputStream();
-    }
-
-    /* InputStream을 전달받아 문자열로 변환 후 반환 */
-    private String readStreamToString(InputStream stream) throws IOException{
-        StringBuilder result = new StringBuilder();
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
-
-        String readLine;
-        while((readLine = br.readLine()) != null) {
-            result.append(readLine + "\n\r");
-        }
-
-        br.close();
-
-        return result.toString();
-    }
 
 
 
