a99cc149c846eee9865c23b4376773dab5f4b2ed
Dongmin <ehdals45454@naver.com>
2024-03-17 17:05:20 +0900

    fix: dto 변수명 카멜케이스로 변환

--- InjeRecipe/src/main/java/com/example/injerecipe/dto/request/RecipeBoardRequest.java
+++ InjeRecipe/src/main/java/com/example/injerecipe/dto/request/RecipeBoardRequest.java
@@ -11,26 +11,26 @@ import java.util.List;
 public class RecipeBoardRequest {
 
     @Schema(description = "일련번호", example = "33")
-    private String recipe_seq;
+    private String recipeSeq;
 
     @Schema(description = "레시피 이름", example = "된장찌개")
-    private String recipe_nm;
+    private String recipeNm;
 
     @Schema(description = "조리 방법", example = "끓이기")
-    private String recipe_way;
+    private String recipeWay;
 
     @Schema(description = "음식 종류", example = "국&찌개")
-    private String recipe_pat;
+    private String recipePat;
 
     @Schema(description = "칼로리", example = "197")
-    private String recipe_eng;
+    private String recipeEng;
 
     @Schema(description = "이미지", example = "1")
-    private String recipe_file_s;
+    private String recipeFileS;
 
     @Schema(description = "재료 정보", example = "두부 20g(2×2×2cm), 애느타리버섯 20g(4가닥), 감자 10g(4×3×1cm), 양파 10g(2×1cm), 대파 10g(5cm), 된장 5g(1작은술), 물 300ml(1½컵)")
-    private String recipe_parts_dtls;
+    private String recipePartsDtls;
 
-    private List<MultipartFile> recipe_images;
-    private List<String> recipe_manuals;
+    private List<MultipartFile> recipeImages;
+    private List<String> recipeManuals;
 }
