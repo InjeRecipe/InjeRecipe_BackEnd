6be8422aaa4fae34077d8ba1cb05e7a73ec446a5
Dongmin <ehdals45454@naver.com>
2024-03-24 22:43:40 +0900

    fix: null 값 예외처리, 커스텀 쿼리 작성

--- InjeRecipe/src/main/java/com/example/injerecipe/dto/request/RecipeSearchRequest.java
+++ InjeRecipe/src/main/java/com/example/injerecipe/dto/request/RecipeSearchRequest.java
@@ -1,19 +1,15 @@
 package com.example.injerecipe.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@Getter
+import java.util.List;
+
+@Data
 public class RecipeSearchRequest {
-    @Schema(description = "검색어", example = "된장찌개")
-    String keyword1;
-    String keyword2;
-    String keyword3;
-    String keyword4;
-    String keyword5;
-    String keyword6;
-    String keyword7;
-    String keyword8;
+    @Schema(description = "검색어 목록", example = "[\"된장찌개\", \"김치찌개\"]")
+    List<String> keywords;
 }
+
--- InjeRecipe/src/main/java/com/example/injerecipe/dto/response/RecipeSearchResponse.java
+++ InjeRecipe/src/main/java/com/example/injerecipe/dto/response/RecipeSearchResponse.java
@@ -1,7 +1,9 @@
 package com.example.injerecipe.dto.response;
 
 import com.example.injerecipe.entity.Recipe;
+import com.example.injerecipe.entity.RecipeBoard;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 
 import java.util.List;
@@ -24,9 +26,28 @@ public class RecipeSearchResponse {
     private String recipePartsDtls;
 
     private List<String> recipeImages;
+
     private List<String> recipeManuals;
 
+    private String errorMessage;
+
     public static RecipeSearchResponse from(Recipe recipe){
+
+        return RecipeSearchResponse.builder()
+                .recipeSeq(recipe.getRecipeSeq())
+                .recipeNm(recipe.getRecipeNm())
+                .recipeWay(recipe.getRecipeWay())
+                .recipePat(recipe.getRecipePat())
+                .recipeEng(recipe.getRecipeEng())
+                .recipeFileS(recipe.getRecipeFileS())
+                .recipePartsDtls(recipe.getRecipePartsDtls())
+                .recipeImages(recipe.getRecipeImages())
+                .recipeManuals(recipe.getRecipeManuals())
+                .build();
+    }
+
+    public static RecipeSearchResponse from(RecipeBoard recipe) {
+
         return RecipeSearchResponse.builder()
                 .recipeSeq(recipe.getRecipeSeq())
                 .recipeNm(recipe.getRecipeNm())
